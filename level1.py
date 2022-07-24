7b4fd32e687c690d217e1eb4e7e820f72a93faad
hayleyshim <yhshim17@gmail.com>
2022-07-25 00:54:27 +0900

    map(),input(),split(),int()

--- level1.py
+++ level1.py
@@ -8,3 +8,23 @@ print(A/B)
 
 A, B = input().split()
 print(int(A)/int(B))
+
+
+#N)10869
+A, B = input().split()
+A = int(A)
+B = int(B)
+
+print(A+B)
+print(A-B)
+print(A*B)
+print(int(A/B))
+print(A%B)
+
+#N)10926
+A = input()
+print(A+"??!")
+
+#N)18108
+y = int(input())
+print(y-543)
