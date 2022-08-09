64d418c147bf9518da2762ac14e9b5138102690a
hayleyshim <yhshim17@gmail.com>
2022-08-10 00:32:29 +0900

    f string

--- level3.py
+++ level3.py
@@ -24,10 +24,19 @@ for i in range(n+1):
 
 print(sum)"""
 
+"""
 #11021
 t = int(input()) # 테스트 케이스 개수 t를 입력받음
 
 for i in range(t): # t 만큼 반복K
     a,b = map(int,input().split()) #split 함수를 이용해서 # 입력받은 값을 공백을 기준으로 분리 정수로 변환하기 위해서 map 함수를 이용하여 코드를 한 줄로 작성
     print("Case #", i+1, ":", a+b, sep='')
+"""
 
+
+#11022
+t = int(input())
+
+for i in range(t):
+    a,b = map(int, input().split())
+    print(f"Case #{i+1}: {a} + {b} = {a+b}")
