94165dac96b18d84c08fdf4e9715f1f1c6e460d3
hayleyshim <yhshim17@gmail.com>
2022-08-27 00:50:41 +0900

    f(n-1)+f(n-2), dp[0]

--- level15.py
+++ level15.py
@@ -13,8 +13,27 @@ fibonacci(n) {
         f[i] <- f[i - 1] + f[i - 2];  # 코드2
     return f[n];
 }
-"""
 
 fib(n){
     return 1 if(n=1 or n=2) else (f[n] = f[n-1]+f[n-2];
 }
+"""
+
+def f(n):
+    if n==1 or n==2:
+        return 1
+    else:
+        return f(n-1)+f(n-2)
+
+def fibonacci(n):
+    dp=[0]*(n+1)
+    dp[1],dp[2]=1, 1
+    cnt2=0
+
+    for i in range(3,n+1):
+        cnt2+=1
+        dp[i]=dp[i-1]+dp[i-2]
+    return cnt2
+
+n = int(input())
+print(f(n),fibonacci(n))
