44d9313f2f4b6fe727478c29eecf418481e593b6
hayleyshim <yhshim17@gmail.com>
2022-08-29 23:46:29 +0900

    sys.stdin.readline()

---
+++ level18.py
@@ -0,0 +1,42 @@
+"""
+#10828
+첫째 줄에 주어지는 명령의 수 N (1 ≤ N ≤ 10,000)이 주어진다.
+둘째 줄부터 N개의 줄에는 명령이 하나씩 주어진다
+
+push X: 정수 X를 스택에 넣는 연산이다.
+pop: 스택에서 가장 위에 있는 정수를 빼고, 그 수를 출력한다.
+만약 스택에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+size: 스택에 들어있는 정수의 개수를 출력한다.
+empty: 스택이 비어있으면 1, 아니면 0을 출력한다.
+top: 스택의 가장 위에 있는 정수를 출력한다.
+만약 스택에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+
+입출력 속도 비교 : sys.stdin.readline > raw_input() > input()
+"""
+
+import sys
+n = int(sys.stdin.readline())
+
+stack=[]
+for i in range(n):
+    command = sys.stdin.readline().split()
+
+    if command[0]=='push':
+        stack.append(command[1])
+    elif command[0]=='pop':
+        if len(stack)==0:
+            print(-1)
+        else:
+            print(stack.pop())
+    elif command[0] == 'size':
+        print(len(stack))
+    elif command[0] == 'empty':
+        if len(stack)==0:
+            print(1)
+        else:
+            print(0)
+    elif command[0] == 'top':
+        if len(stack)==0:
+            print(-1)
+        else:
+            print(stack[-1])
