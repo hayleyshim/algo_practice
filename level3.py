7dbf0dc28067a62a45e3f141fd3aeaf5018c398c
hayleyshim <yhshim17@gmail.com>
2022-09-03 00:11:07 +0900

    arr[i][0]*arr[i][1]

--- level3.py
+++ level3.py
@@ -62,3 +62,34 @@ while True:
         break
     else:
         print(a + b)
+
+#25304
+"""
+영수증에 적힌,
+
+- 구매한 각 물건의 가격과 개수
+- 구매한 물건들의 총 금액
+
+을 보고, 구매한 물건의 가격과 개수로 계산한 총 금액이 영수증에 적힌 총 금액과 일치하는지 검사
+
+첫째 줄에는 영수증에 적힌 총 금액 $X$가 주어진다.
+
+둘째 줄에는 영수증에 적힌 구매한 물건의 종류의 수 $N$이 주어진다.
+
+이후 $N$개의 줄에는 각 물건의 가격 $a$와 개수 $b$가 공백을 사이에 두고 주어진다.
+
+"""
+
+x = int(input())
+n = int(input())
+arr = [list(map(int, input().split())) for _ in range(n)]
+
+sum = 0
+
+for i in range(n):
+    sum += arr[i][0]*arr[i][1] # 0번 상품의 가격(arr[0][0]) * 개수(arr[0][1])
+
+if x == sum:
+    print("Yes")
+else:
+    print("No")
