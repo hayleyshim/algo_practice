4bd08ef2f5d3a1e5ee300ea60adac11f095cba1d
hayleyshim <yhshim17@gmail.com>
2022-09-03 22:50:13 +0900

    sum(map(int, input(n)))

--- level6.py
+++ level6.py
@@ -6,3 +6,13 @@
 print(ord(input())) #ord : 문자열 -> 아스키
 print(chr(input())) #chr : 아스키 -> 문자열
 
+#11720
+"""
+N개의 숫자가 공백 없이 쓰여있다. 
+이 숫자를 모두 합해서 출력하는 프로그램을 작성
+"""
+
+N = int(input())
+num_list = sum(map(int, input()))
+
+print(num_list)
