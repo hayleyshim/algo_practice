3209332692c96a81aceaf76715b17f35137b455b
hayleyshim <yhshim17@gmail.com>
2022-09-03 22:46:57 +0900

    sum(map(int, str(n)))

--- level5.py
+++ level5.py
@@ -11,3 +11,26 @@ def solve(a):
     for x in a:
         ans += x
     return ans
+
+#4673
+"""
+양의 정수 n에 대해서 d(n)을 n과 n의 각 자리수를 더하는 함수라고 정의하자
+n을 d(n)의 생성자라고 한다.
+
+10000보다 작거나 같은 셀프 넘버를 한 줄에 하나씩 출력하는 프로그램을 작성하시오.
+"""
+
+def d(n):
+    # int로 들어온 n을 형변환 시켜부면 문자열,
+    # 즉. iterable 되고 map 함수를 이용해 int로 바꾸면 각 자리 합 구할 수 있음
+    n = n + sum(map(int, str(n)))
+    return n
+
+nonSelfNum = set()
+
+for i in range(1, 10001):
+    nonSelfNum.add(d(i))
+
+for j in range(1, 10001):
+    if j not in nonSelfNum:
+        print(j)
