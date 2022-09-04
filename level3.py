f0d1ce66b5d936210a11b945c669c091cf12694f
hayleyshim <yhshim17@gmail.com>
2022-09-05 00:58:55 +0900

    num%i==0

--- level3.py
+++ level3.py
@@ -1,20 +1,20 @@
-"""
+
 #N) 2739
 
 n = int(input())
 
 for i in range(1,10):
-    print(n, '*', i, '=', n*i)"""
+    print(n, '*', i, '=', n*i)
 
-"""
+
 #N) 10950
 t = int(input()) # 테스트 케이스 개수 t를 입력받음
 
 for i in range(t): # t 만큼 반복
     a,b = map(int,input().split()) #split 함수를 이용해서 # 입력받은 값을 공백을 기준으로 분리 정수로 변환하기 위해서 map 함수를 이용하여 코드를 한 줄로 작성
-    print(a+b)"""
+    print(a+b)
 
-"""
+
 #8393
 n = int(input())
 sum = 0
@@ -22,27 +22,23 @@ sum = 0
 for i in range(n+1):
     sum += i
 
-print(sum)"""
+print(sum)
 
-"""
+
 #11021
 t = int(input()) # 테스트 케이스 개수 t를 입력받음
 
 for i in range(t): # t 만큼 반복K
     a,b = map(int,input().split()) #split 함수를 이용해서 # 입력받은 값을 공백을 기준으로 분리 정수로 변환하기 위해서 map 함수를 이용하여 코드를 한 줄로 작성
     print("Case #", i+1, ":", a+b, sep='')
-"""
 
-"""
 #11022
 t = int(input())
 
 for i in range(t):
     a,b = map(int, input().split())
     print(f"Case #{i+1}: {a} + {b} = {a+b}")
-"""
 
-"""
 #10951
 
 while True:
@@ -51,7 +47,7 @@ while True:
     except:
         break
     print(a+b)
-"""
+
 
 #10952
 
--- level7.py
+++ level7.py
@@ -63,3 +63,34 @@ while n > room:
     room = room + (6*cnt)
     cnt += 1
 print(cnt)
+
+#2581
+"""
+자연수 M과 N이 주어질 때 M이상 N이하의 자연수 중 소수인 것을 모두 골라 
+이들 소수의 합과 최솟값을 찾는 프로그램을 작성
+
+M이상 N이하의 자연수 중 소수인 것을 모두 찾아 첫째 줄에 그 합을, 
+둘째 줄에 그 중 최솟값을 출력한다. 
+
+단, M이상 N이하의 자연수 중 소수가 없을 경우는 첫째 줄에 -1을 출력한다.
+"""
+
+start = int(input())
+last = int(input())
+sosu = []
+
+for num in range(start, last+1):
+    error = 0
+    if num > 1:
+        for i in range(2, num):
+            if num % i == 0:
+                error += 1
+                break
+        if error == 0 :
+            sosu.append(num)
+
+if len(sosu) > 0:
+    print(sum(sosu))
+    print(min(sosu))
+else:
+    print(-1)
