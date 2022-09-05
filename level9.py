1d177a1e20a85187f9fc0f37979a4a7cf242c770
hayleyshim <yhshim17@gmail.com>
2022-09-06 01:04:39 +0900

    list(map(int, str(i)))

--- level9.py
+++ level9.py
@@ -45,12 +45,15 @@ print(ans)
 N = int(input())
 result = 0
 
-for i in range(1 , N+1):
-    a = list(map(int, str(i)))
-    s = i + sum(a)
-    if(s == N):
-        result = i
+for i in range(1, N+1):
+    A = list(map((int, str(i))))
+    result = i + sum(A)
+
+    if result == N:
+        print(i)
         break
-print(result)
+    if i == N:
+        print(0)
+
 
 
