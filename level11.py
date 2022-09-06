d96ea7cff832353766752b6e46f5877d8d8672e4
hayleyshim <yhshim17@gmail.com>
2022-09-06 23:31:49 +0900

    binary search()

--- level11.py
+++ level11.py
@@ -22,3 +22,37 @@ for _ in range(M):
 
 print(count)
 
+#10815
+"""
+상근이는 숫자 카드 N개를 가지고 있다. 
+정수 M개가 주어졌을 때, 
+이 수가 적혀있는 숫자 카드를 상근이가 가지고 있는지 아닌지를 구하는 프로그램을 작성
+binary search()
+"""
+import sys
+
+n = int(input())
+card = list(map(int, sys.stdin.readline().split()))
+m = int(input())
+check = list(map(int, sys.stdin.readline().split()))
+
+card.sort()
+
+def binary_search(array, target, start, end):
+    while start <= end:
+        mid = (start + end) // 2
+
+        if array[mid] == target:
+            return mid
+        elif array[mid] > target:
+            end = mid - 1
+        else:
+            start = mid + 1
+    return None
+
+
+for i in range(m):
+    if binary_search(card, check[i], 0, n - 1) is not None:
+        print(1, end=' ')
+    else:
+        print(0, end=' ')
