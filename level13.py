08e51fcd2528bb8e107f9e65550de71ca1aed26d
hayleyshim <yhshim17@gmail.com>
2022-09-07 04:07:44 +0900

    n_max*n_min

--- level13.py
+++ level13.py
@@ -15,3 +15,19 @@ while True:
         print('neither')
 
 
+#1037
+"""
+양수 A가 N의 진짜 약수가 되려면, N이 A의 배수이고, A가 1과 N이 아니어야 한다. 
+어떤 수 N의 진짜 약수가 모두 주어질 때, N을 구하는 프로그램을 작성
+
+첫째 줄에 N의 진짜 약수의 개수가 주어진다. 
+둘째 줄에는 N의 진짜 약수가 주어진다.
+
+"""
+
+N = int(input())
+M = list(map(int, input().split()))
+
+N_max = max(M)
+N_min = min(M)
+print(N_max*N_min)
