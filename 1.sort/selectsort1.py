f4d8a0779a1176e23a38897e9635974f34f6e10b
hayleyshim <yhshim17@gmail.com>
2022-10-10 00:00:25 +0900

    selectsort

--- 1.sort/selectsort1.py
+++ 1.sort/selectsort1.py
@@ -19,3 +19,26 @@ def selectsort(tmplist):
 tmplist = [10, 2, 6, 4, 3, 7, 5]
 selectsort(tmplist)
 print(tmplist)
+
+
+print('--------------------------')
+
+def swap(data, a, b):
+    global cnt
+    tmp = data[a]
+    data[a] = data[b]
+    data[b] = tmp
+
+def selectsort(tmplist):
+    for i in range(len(tmplist)):
+        minidx = i
+
+    for j in range(i+1, len(tmplist)):
+        if tmplist[j] < tmplist[minidx]:
+            minidx = j
+    if minidx != i:
+        swap(tmplist, minidx, i)
+
+tmplist = [10, 2, 5, 4, 6]
+selectsort(tmplist)
+print(tmplist)
